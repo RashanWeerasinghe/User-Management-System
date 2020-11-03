ce296fc30c37e0a6b52d22cfee1decae20068bb9
malithweerasinghe <malithrashan03@gmail.com>
2020-11-03 14:37:47 +0530

    update

--- inc/connection.php
+++ inc/connection.php
@@ -11,4 +11,5 @@
 	if (mysqli_connect_errno()) {
 		die('Database connection failed ' . mysqli_connect_error());
 	}
+
 ?>
--- index.php
+++ index.php
@@ -1,117 +1,93 @@
+<?php session_start(); ?>
 <?php require_once('inc/connection.php'); ?>
-<?php
+<?php 
 
-$errors=array();
-//check for form submission
-if(isset($_POST['submit'])){
-// check if the y=username and password has beeb enterd
-if(!isset($_POST['email'])||strlen(trim($_POST['email']))<1){
-     $errors[]='useranme is missing/Invaild';
+	// check for form submission
+	if (isset($_POST['submit'])) {
 
+		$errors = array();
 
-}
-if(!isset($_POST['password'])||strlen(trim($_POST['password']))<1){
-     $errors[]='password is missing/Invaild';
+		// check if the username and password has been entered
+		if (!isset($_POST['email']) || strlen(trim($_POST['email'])) < 1 ) {
+			$errors[] = 'Username is Missing / Invalid';
+		}
 
+		if (!isset($_POST['password']) || strlen(trim($_POST['password'])) < 1 ) {
+			$errors[] = 'Password is Missing / Invalid';
+		}
 
-}
-// check if there are any errors in the form
-if(empty($errors)){
- 
-// save username and password into variables
-     $email=mysqli_real_escape_string($connection,$_POST['email']);
-      $password=mysqli_real_escape_string($connection,$_POST['password']);
-      $hashed_password=sha1($password)
+		// check if there are any errors in the form
+		if (empty($errors)) {
+			// save username and password into variables
+			$email 		= mysqli_real_escape_string($connection, $_POST['email']);
+			$password 	= mysqli_real_escape_string($connection, $_POST['password']);
+			$hashed_password = sha1($password);
 
-     // prepare database query
-      $query = "SELECT *FROM user
-              WHERE email='{$email}'
-              AND password ='{$hashed_password}' 
-              LIMIT 1  ";
+			// prepare database query
+			$query = "SELECT * FROM user 
+						WHERE email = '{$email}' 
+						AND password = '{$hashed_password}' 
+						LIMIT 1";
 
-       $result_set=mysqli_query($connection,$query);
-       if($result_set) {
+			$result_set = mysqli_query($connection, $query);
 
- //query successfull
+			if ($result_set) {
+				// query succesfful
 
-          if(mysqli_num_rows($result_set)==1){
-
-
-               //valid user found
-               // check if the user is vaild
-               // redirect to users.php
-               header('location: users.php');
-          }
-          else {
-               //invild user found
-               $errors[]='invaild username/password';
-          }
-
-       }else{
-
-$errors[]='database query faild';
-
-       }      
-  
-}
-  
-}
-
-
+				if (mysqli_num_rows($result_set) == 1) {
+					// valid user found
+					// redirect to users.php
+					header('Location: users.php');
+				} else {
+					// user name and password invalid
+					$errors[] = 'Invalid Username / Password';
+				}
+			} else {
+				$errors[] = 'Database query failed';
+			}
+		}
+	}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Log In - User Management System</title>
-     <link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 	<div class="login">
-   <form action="index.php" method="post">
-     <fieldset>
-     <legend>
-     	<h1> Log In</h1>
-          </legend> 
-          <?php
 
-          if(isset($errors)&& !empty($errors)){
+		<form action="index.php" method="post">
+			
+			<fieldset>
+				<legend><h1>Log In</h1></legend>
 
-               echo '<p class="error">Invalid Username / password</p>';
-          }
+				<?php 
+					if (isset($errors) && !empty($errors)) {
+						echo '<p class="error">Invalid Username / Password</p>';
+					}
+				?>
 
-               ?>
-          
-     	<p>
-     		
-     		<label for="">username:</label>
-     		<input type="text" name="email" id="" placeholder="email address">
+				<p>
+					<label for="">Username:</label>
+					<input type="text" name="email" id="" placeholder="Email Address">
+				</p>
 
+				<p>
+					<label for="">Password:</label>
+					<input type="password" name="password" id="" placeholder="Password">
+				</p>
 
-     	</p>
-     	<p>
-     		
-     		<label for="">password</label>
-     		<input type="password" name="password" id="" placeholder="password">
-     	</p>
+				<p>
+					<button type="submit" name="submit">Log In</button>
+				</p>
 
-     	<p>
-     		<button type="submit" name="submit">log In</button>
+			</fieldset>
 
+		</form>		
 
-     	</p>
-
-
-     
-      
-
-
-
-     </fieldset>
-
-
-	</div> 
+	</div> <!-- .login -->
 </body>
 </html>
 <?php mysqli_close($connection); ?>
